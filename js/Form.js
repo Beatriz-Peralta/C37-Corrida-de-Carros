5e914575989cd63510e430e000a65add6abac126
Beatriz-Peralta <114191631+Beatriz-Peralta@users.noreply.github.com>
2023-03-14 18:55:38 -0300

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -1,25 +1,43 @@
 class Form {
   constructor() {
-    // CRIE OS ELEMENTOS DA CLASSE
+    this.input = createInput("").attribute("placeholder", "Digite seu nome");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "título do jogo");
+    this.greeting = cretaeElement("h2");
   }
 
   setElementPosition() {
-    // DEFINA A POSIÇÃO DOS ELEMENTOS
+    this.titleImg.position(120,100);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+    
   }
 
 
   hide() {
    // USE A FUNÇÃO PARA DESAPARECER OS ELEMENTOS
+    this.greetinghide();
+    this.playButton.hide();
+    this.input.hide();
   }
 
   handleMousePressed() {
    // CRIE A FUNÇÃO DE PRESSIONAMENTO DE TECLA
-
-   /* O símbolo usado para escrever a mensagem abaixo (laranja) é a crase. */
+   this.playButton.mousePressed(() => {
+     this.input.hide();
+     this.playButton.hide();
+     var message = Olá ${this.input.value()} </br>espere outro jogador entrar...
+     this.greeting.html(message);
+     player.name = this.input.value();
+     player.index = 1;      }); }
+     /* O símbolo usado para escrever a mensagem abaixo (laranja) é a crase. */
   }
 
   display() {
     // CHAME A FUNÇÃO DE POSIÇÃO DOS ELEMENTOS
+    this.setElementsPosition();
     // CHAME A FUNÇÃO DE PRESSIONAMENTO DE TECLA
+    this.handleMousePressed();
   }
 }
